20076e3ef7bec203f880bf0b0c2f566cf2cb8ccd
Ali Mohammad Movahedian <am.movahedian1998@gmail.com>
2021-03-12 00:34:49 +0330

    Add retrofit network calls

---
+++ admediator/src/main/java/com/example/admediator/service/ServiceBuilder.kt
@@ -0,0 +1,19 @@
+package com.example.admediator.service
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private const val BASE_URL: String = "http://123.123.123.123:4444/api/"
+
+    private val retrofit = Retrofit.Builder()
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
