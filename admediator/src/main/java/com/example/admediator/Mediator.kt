1114411779a7513b097f91d8f205e9043dade0cf
Ali Mohammad Movahedian <am.movahedian1998@gmail.com>
2021-03-13 00:34:58 +0330

    Add ad request functionality for tapsell and chartboost

--- admediator/src/main/java/com/example/admediator/Mediator.kt
+++ admediator/src/main/java/com/example/admediator/Mediator.kt
@@ -5,22 +5,33 @@ import android.content.Context
 import android.util.Log
 import com.chartboost.sdk.Chartboost
 import com.example.admediator.constants.AdNetwork
-import com.example.admediator.constants.ZoneType
 import com.example.admediator.data.AdNetworkEntity
+import com.example.admediator.data.AdResponse
+import com.example.admediator.data.ZoneConfigEntity
+import com.example.admediator.listeners.AdRequestListener
+import com.example.admediator.listeners.InitializeListener
+import com.example.admediator.networks.ChartboostUtil
+import com.example.admediator.networks.TapsellUtil
 import com.example.admediator.repository.AdRepository
 import com.unity3d.ads.UnityAds
+import io.reactivex.Maybe
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import ir.tapsell.sdk.Tapsell
+import java.util.concurrent.TimeUnit
+
 
 class Mediator {
 
     private val CB_SIGNATURE = "dummy_signature"
 
     private val adRepository = AdRepository()
+
     private lateinit var networks: List<AdNetworkEntity>
+
+    private var availableAdNetwork = ""
 
     fun initialize(application: Application, appId: String, listener: InitializeListener){
         adRepository.getAdNetworks(appId)
@@ -49,5 +60,66 @@ class Mediator {
                     UnityAds.initialize(application, net.appId)
             }
         }
+    }
+
+    fun requestAd(context: Context, zoneId: String, listener: AdRequestListener){
+        adRepository.initSharedPreferences(context)
+        adRepository.getZoneConfig(zoneId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({zoneConfig ->
+                zoneConfigTimeout(zoneId, zoneConfig.timeout)
+                requestAdFromWaterfall(context, zoneConfig, listener)
+            }, {
+                Log.e("Mediator", "RequestAdError:".plus(it.message))
+                listener.onError(it.message!!)
+            }).also {
+                CompositeDisposable(it)
+            }
+        Context.WINDOW_SERVICE
+    }
+
+    private fun zoneConfigTimeout(zoneId: String, timeout: Long){
+        adRepository.removeZoneConfig(zoneId)
+            .delaySubscription(timeout, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    private fun requestAdFromWaterfall(context: Context, zoneConfig: ZoneConfigEntity, listener: AdRequestListener){
+        val singles = mutableListOf<Single<AdResponse>>()
+        zoneConfig.waterfall.forEach {netConfig ->
+            when(netConfig.adNetwork){
+                AdNetwork.TAPSELL -> {
+                    singles.add(Single.just(
+                        TapsellUtil.requestAd(context, netConfig.zoneId, listener)
+                    ).timeout(netConfig.timeout, TimeUnit.MILLISECONDS))
+                }
+                AdNetwork.CHARTBOOST ->{
+                    singles.add(Single.just(
+                        ChartboostUtil.requestAd(netConfig.zoneId, zoneConfig.zoneType, listener)
+                    ))
+                }
+
+//                AdNetwork.UNITY_ADS ->
+            }
+        }
+
+        Single.concat(singles)
+            .firstElement()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .flatMap{
+                res -> adRepository.saveAdId(res.adId)
+                Maybe.just(res)
+            }
+            .subscribe({response ->
+                availableAdNetwork = response.adNetwork
+            }, {
+                Log.e("Mediator", "RequestAdError:".plus(it.message))
+            }).also {
+                CompositeDisposable(it)
+            }
     }
 }
---
+++ admediator/src/main/java/com/example/admediator/data/AdResponse.kt
@@ -0,0 +1,6 @@
+package com.example.admediator.data
+
+data class AdResponse (
+    val adNetwork: String,
+    val adId: String
+)
