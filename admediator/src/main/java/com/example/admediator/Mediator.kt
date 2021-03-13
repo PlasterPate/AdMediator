bc751e9340c46ad1397a97266ff7dd3c71ea099c
Ali Mohammad Movahedian <am.movahedian1998@gmail.com>
2021-03-13 17:34:29 +0330

    Refactor mediator by implementing factory pattern

--- admediator/src/main/java/com/example/admediator/Mediator.kt
+++ admediator/src/main/java/com/example/admediator/Mediator.kt
@@ -4,16 +4,14 @@ import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.util.Log
-import com.example.admediator.constants.AdNetwork
 import com.example.admediator.data.AdNetworkEntity
 import com.example.admediator.data.AdState
 import com.example.admediator.data.ZoneConfigEntity
 import com.example.admediator.listeners.AdRequestListener
 import com.example.admediator.listeners.AdShowListener
 import com.example.admediator.listeners.InitializeListener
-import com.example.admediator.networks.ChartboostUtil
-import com.example.admediator.networks.TapsellUtil
-import com.example.admediator.networks.UnityUtil
+import com.example.admediator.networks.BaseNetwork
+import com.example.admediator.networks.NetworkFactory
 import com.example.admediator.repository.AdRepository
 import io.reactivex.Maybe
 import io.reactivex.Single
@@ -27,15 +25,16 @@ class Mediator {
 
     private val adRepository = AdRepository()
 
-    private lateinit var networks: List<AdNetworkEntity>
+    private val networkFactory = NetworkFactory()
+
+    private var networks = mutableMapOf<String, BaseNetwork>()
 
     fun initialize(application: Application, appId: String, listener: InitializeListener){
         adRepository.getAdNetworks(appId)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                networks = it
-                initNetworks(application)
+            .subscribe({netList ->
+                initNetworks(netList, application)
                 listener.onSuccess()
             }, {
                 Log.e("Mediator", "InitializeError:".plus(it.message))
@@ -45,16 +44,11 @@ class Mediator {
             }
     }
 
-    private fun initNetworks(application: Application ){
-        networks.forEach{ net ->
-            when(net.name){
-                AdNetwork.TAPSELL ->
-                    TapsellUtil.initialize(application, net.appId)
-                AdNetwork.CHARTBOOST ->
-                    ChartboostUtil.initialize(application, net.appId)
-                AdNetwork.UNITY_ADS ->
-                    UnityUtil.initialize(application, net.appId)
-            }
+    private fun initNetworks(networkList: List<AdNetworkEntity>, application: Application ){
+        networkList.forEach{ adNet ->
+            val networkInstance = networkFactory.createNetwork(adNet)
+            networkInstance.initialize(application)
+            networks[adNet.name] = networkInstance
         }
     }
 
@@ -86,22 +80,10 @@ class Mediator {
     private fun requestAdFromWaterfall(context: Context, zoneConfig: ZoneConfigEntity, listener: AdRequestListener){
         val singles = mutableListOf<Single<AdState>>()
         zoneConfig.waterfall.forEach {netConfig ->
-            when(netConfig.adNetwork){
-                AdNetwork.TAPSELL -> {
-                    singles.add(Single.just(
-                        TapsellUtil.requestAd(context, netConfig.zoneId, listener)
-                    ).timeout(netConfig.timeout, TimeUnit.MILLISECONDS))
-                }
-                AdNetwork.CHARTBOOST ->{
-                    singles.add(Single.just(
-                        ChartboostUtil.requestAd(netConfig.zoneId, zoneConfig.zoneType, listener)
-                    ))
-                }
-                AdNetwork.UNITY_ADS -> {
-                    singles.add(Single.just(
-                        UnityUtil.requestAd(listener)
-                    ))
-                }
+            if (netConfig.adNetwork in networks.keys){
+                singles.add(Single.just(
+                    networks[netConfig.adNetwork]!!.requestAd(context, netConfig.zoneId, zoneConfig.zoneType, listener)
+                ).timeout(netConfig.timeout, TimeUnit.MILLISECONDS))
             }
         }
 
@@ -120,33 +102,18 @@ class Mediator {
             }
     }
 
-
     fun showAd(activity: Activity, zoneId: String, listener: AdShowListener){
         adRepository.getAdState()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({state ->
-                if (state.id != ""){
-                    showAdFromNetwork(activity, zoneId, state, listener)
+                if (state.id != "" && state.network in networks.keys){
+                    networks[state.network]!!.showAd(activity, state, zoneId, listener)
                 }
             }, {
                 Log.e("Mediator", "ShowAdError:".plus(it.message))
             }).also {
                 CompositeDisposable(it)
             }
-    }
-
-    private fun showAdFromNetwork(activity: Activity, zoneId: String, adState: AdState, listener: AdShowListener){
-        when(adState.network){
-            AdNetwork.TAPSELL ->{
-                TapsellUtil.showAd(activity, zoneId, adState.id, listener)
-            }
-            AdNetwork.CHARTBOOST ->{
-                ChartboostUtil.showAd(adState, listener)
-            }
-            AdNetwork.UNITY_ADS ->{
-                UnityUtil.showAd(activity, adState.id, listener)
-            }
-        }
     }
 }
---
+++ admediator/src/main/java/com/example/admediator/networks/NetworkFactory.kt
@@ -0,0 +1,20 @@
+package com.example.admediator.networks
+
+import com.example.admediator.constants.AdNetwork
+import com.example.admediator.data.AdNetworkEntity
+import java.lang.IllegalArgumentException
+
+internal class NetworkFactory {
+
+    fun createNetwork(adNetworkEntity: AdNetworkEntity) : BaseNetwork{
+        return when(adNetworkEntity.name){
+            AdNetwork.TAPSELL ->
+                TapsellNetwork(adNetworkEntity.appId)
+            AdNetwork.CHARTBOOST ->
+                ChartboostNetwork(adNetworkEntity.appId)
+            AdNetwork.UNITY_ADS ->
+                UnityNetwork(adNetworkEntity.appId)
+            else -> throw IllegalArgumentException("There is no ${adNetworkEntity.name} network available")
+        }
+    }
+}
